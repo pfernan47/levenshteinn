71b636f1d456eafd33a767a34fe4740327601e5b
julianavdiez <jvalle22@eafit.edu.co>
2017-08-18 14:33:19 -0500

    Update Levenshtein.hs

--- Levenshtein.hs
+++ Levenshtein.hs
@@ -5,6 +5,9 @@ import Data.Map
 import Data.Set
 import Data.List
 
+-- *Levenshtein function takes the first element of v list, and creates a list
+-- with the values of truth for each word in v. Its parameters are a word w, an
+-- int n and a list of strings [v]
 levenshtein :: String -> Int -> [String] -> [Bool]
 levenshtein w n (x:xs) = [if ((length x )<= (length w +n) )
                           then (oneWord w n x) else (False)]
@@ -12,31 +15,44 @@ levenshtein w n (x:xs) = [if ((length x )<= (length w +n) )
 levenshtein w n []  = []
 
 -- "oneWord" determines if the word v is in an accepting state
--- at the ENFA built with w and n    
+-- at the ENFA built with w and n
 oneWord :: (Num t, Ord a, Ord t) => [a] -> t -> [a] -> Bool
 oneWord w n v
   |Data.Set.null (Data.Set.intersection (accepting(buildEnfa w n))
    (evaluateEnfa v (buildEnfa w n)))                                  = False
   |otherwise                                                          = True
 
+-- "isFinalState" determinates if the current state (i,j) is located at the right 
+-- upper corner of the Levenshtein automaton constructed with the word w and the 
+-- maximun distance n, based on the tuple contained in each state.
 isFinalState :: (Eq a1, Foldable t) => (Int, a1) -> t a -> a1 -> Bool
 isFinalState (i,j) w n
   |i== (length w) && j==n     =True
   |otherwise                  =False
 
+-- "isUpperState" determinates if the current state is located on the highest 
+-- row of the Levenshtein automaton with reference w and degree n,  except for 
+-- the state at the upper right corner
 isUpperState :: (Foldable t, Eq a1) => (Int, a1) -> t a -> a1 -> Bool
 isUpperState (i,j) w n
   |j==n && i/= (length w)     =True
   |otherwise                  =False
-
+  
+-- "isRightState" determinates if the current state is located at the last 
+--column the Levenshtein automaton with word and degree n, except for the 
+-- state at the upper right corner
 isRightState :: (Eq a1, Foldable t) => (Int, a1) -> t a -> a1 -> Bool
 isRightState (i,j) w n
   |i== (length w) && j /=n    =True
   |otherwise                  =False
 
+-- "buildEnfa" construct the Levenshtein automaton, "floor" by "floor"
 buildEnfa :: (Ord a1, Ord a, Num a) => [a1] -> a -> ENFA (Int, a) a1
 buildEnfa w n = buildState (0,0) w w n (initENFA (0,0))
 
+-- "buildState" construct the states for the Levenshtein automaton with its 
+-- respectives transitions, defining if a transition is allowed or not and 
+-- creating the acceptation states set
 buildState :: (Num a1, Ord a1, Ord a) => (Int, a1) -> [a] -> [a] -> a1 
               -> ENFA (Int, a1) a -> ENFA (Int, a1) a
 buildState (i,j) [] w n e
@@ -64,7 +80,8 @@ evaluateEnfa :: (Num t, Num t1, Ord a, Ord t, Ord t1) =>
                 [a] -> ENFA (t1, t) a -> Set (t1, t)
 evaluateEnfa w e = evaluate w e (0,0)
 
-
+-- "evaluate" execute the transitions, identifying its kind according to the 
+-- entrance received by the state
 evaluate ::(Ord t1, Ord t, Ord a)=>[a]->ENFA (t1, t) a-> (t1, t) -> Set (t1, t)
 evaluate [] e (i,j)
   |jump (i,j) Eps e        =evaluate [] e (Data.Set.elemAt 0
